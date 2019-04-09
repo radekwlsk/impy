bf78a8fcd88db57fdad7f25687fcdda569fa5b09
Radosław Kowalski <radek.kwlsk@gmail.com>
2019-04-09 16:23:39 +0200

    rename app to 'movies_api'

--- src/internet_movie_pythonbase/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class InternetMoviePythonbaseConfig(AppConfig):
-    name = 'src.internet_movie_pythonbase'
---
+++ src/movies_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MoviesAPIConfig(AppConfig):
+    name = 'movies_api'
