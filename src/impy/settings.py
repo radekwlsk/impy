e0eda1dd72755ec1058fd3e4a888e1b64b540956
Radosław Kowalski <radek.kwlsk@gmail.com>
2019-04-09 16:21:56 +0200

    set proper database

--- src/impy/settings.py
+++ src/impy/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'impy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': '5432',
     }
 }
 
